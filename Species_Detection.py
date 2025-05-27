30316550c234d82d59fd2b11adff8c7a5d92ba30
Adam Hetherwick <adamhetherwick@gmail.com>
2025-05-27 10:51:46 -0700

    Create Species_Detection.py

---
+++ Species_Detection.py
@@ -0,0 +1,17 @@
+"""
+This project provides species predictions for Urban Wildlife photos provided by the Oakland Zoo.
+Created by Adam Hetherwick with slight guidance from Sammantha Sammons and Darren Minier.
+Started 05/27/2025
+"""
+
+import pandas
+import numpy
+import matplotlib
+import sklearn
+
+
+
+def main() -> None:
+    pass
+
+
